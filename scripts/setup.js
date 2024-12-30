b752667b4153247ccd8118bde9517922fd453df9
mishagordeev <onitself@gmail.com>
2024-12-30 13:00:49 +0300

    Add Files

---
+++ scripts/setup.js
@@ -0,0 +1,35 @@
+Hooks.once("ready", async () => {
+    console.log("Macro Compendium Module | Готово!");
+
+    const pack = game.packs.get("macro-compendium-module.macros");
+    if (!pack) {
+        console.error("Macro Compendium Module | Компедиум не найден.");
+        return;
+    }
+
+    const alreadyCreated = await pack.getDocuments();
+    if (alreadyCreated.length > 0) {
+        console.log("Macro Compendium Module | Макросы уже добавлены.");
+        return;
+    }
+
+    const macros = [
+        {
+            name: "Send Anonymous Chat",
+            type: "script",
+            command: `
+let message = "Ваше сообщение";
+ChatMessage.create({
+    content: message,
+    speaker: { alias: "Gamemaster" },
+    whisper: ChatMessage.getWhisperRecipients("GM")
+});
+            `,
+            img: "icons/svg/dice-target.svg",
+            permission: { default: 2 }
+        }
+    ];
+
+    await pack.createDocuments(macros);
+    console.log("Macro Compendium Module | Макросы успешно добавлены.");
+});
